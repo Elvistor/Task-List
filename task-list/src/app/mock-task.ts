e183206cac5f2deece9b247b73b10acac99f5fc4
Elvistor <pupuma147@gmail.com>
2022-01-22 11:36:15 -0300

    Agregado mock de tareas

---
+++ task-list/src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task";
+export const TASK:Task[] =[
+    {
+        id:1,
+        text:"Terminar módulo 4, Angular",
+        day:"5 Febrero a las 12:00 hs",
+        reminder:true
+    },
+    {
+        id:2,
+        text:"Terminar Bases de datos",
+        day:"5 Febrero a las 17:00 hs",
+        reminder:false
+    },
+    {
+        id:3,
+        text:"Investigar mas cosas",
+        day:"5 Febrero a las 17:00 hs",
+        reminder:false
+    },
+    {
+        id:4,
+        text:"Leer libro",
+        day:"5 Febrero a las 20:00 hs",
+        reminder:false
+    }
+]
